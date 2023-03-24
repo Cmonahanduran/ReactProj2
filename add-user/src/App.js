c667bc44d7b52fd6b7b0a77ea43b540150141a8e
Cmonahanduran <118871270+Cmonahanduran@users.noreply.github.com>
2023-03-23 22:04:31 -0700

    Start component/Card class

---
+++ add-user/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import AddUser from './Components/AddUser/AddUser';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
